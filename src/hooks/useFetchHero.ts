99b06bb476f37f8fc4ae49ec2cd394b93eff8e96
kwrush <kaiwang.me@gmail.com>
2020-05-16 16:51:50 +0200

    Not fetching new data when it is in context

--- src/hooks/useFetchHero.ts
+++ src/hooks/useFetchHero.ts
@@ -1,16 +1,24 @@
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState, useCallback, useContext } from 'react';
 import { searchSuperHeroById } from '~app/lib/api';
 import { HeroAPI } from '~app/types/response';
+import { HeroContext } from '~app/containers/HeroProvider';
+import { HeroEntity } from '~app/types/app';
 
 const useFetchHero = (heroId: number) => {
   const [hero, setHero] = useState<HeroAPI>();
+  const { heros, searchResult } = useContext(HeroContext);
   const [isLoading, setIsLoading] = useState(true);
 
   const fetchHero = useCallback(
     async (id: number, signal: AbortSignal) => {
       try {
-        const data = await searchSuperHeroById(id, signal);
-        setHero(data);
+        const localHeros = { ...heros, ...searchResult } as HeroEntity;
+        if (localHeros[id]) {
+          setHero(localHeros[id]);
+        } else {
+          const data = await searchSuperHeroById(id, signal);
+          setHero(data);
+        }
       } catch (err) {
         // eslint-disable-next-line no-console
         console.error(err);
@@ -18,7 +26,7 @@ const useFetchHero = (heroId: number) => {
 
       setIsLoading(false);
     },
-    [setHero, setIsLoading],
+    [setHero, heros, searchResult, setIsLoading],
   );
 
   useEffect(() => {
