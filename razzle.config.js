2f6aa886e92eabbe6aa0df0347a2897989a17756
kwrush <kaiwang.me@gmail.com>
2020-05-17 12:17:11 +0200

    Deployment to heroku

--- razzle.config.js
+++ razzle.config.js
@@ -1,5 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
+const razzleHeroku = require('razzle-heroku');
 
 module.exports = {
   plugins: [
@@ -17,7 +18,7 @@ module.exports = {
       },
     },
   ],
-  modify: (defaultConfig, { dev }) => {
+  modify: (defaultConfig, { dev, target }, webpack) => {
     const config = { ...defaultConfig };
 
     config.module.rules = config.module.rules.map((rule) => {
@@ -59,6 +60,6 @@ module.exports = {
       '~app': path.resolve(__dirname, './src'),
     };
 
-    return config;
+    return razzleHeroku(config, { target, dev }, webpack);
   },
 };
--- src/lib/getInitialHeros.ts
+++ src/lib/getInitialHeros.ts
@@ -13,11 +13,13 @@ const HEROS = [
   { id: 506, name: 'thanos' },
   { id: 140, name: 'darkseid' },
   { id: 553, name: 'wonder-woman' },
+  { id: 30, name: 'aquaman' },
   { id: 510, name: 'thor' },
   { id: 481, name: 'star-lord' },
   { id: 550, name: 'wolverine' },
   { id: 160, name: 'doctor-strange' },
   { id: 307, name: 'loki' },
+  { id: 80, name: 'black-widow' },
 ];
 
 // select the given number of heros randomly from HEROS array
