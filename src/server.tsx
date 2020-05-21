c30d366d7b6e42ae1239640f4c8eae16bba631eb
kwrush <kaiwang.me@gmail.com>
2020-05-21 15:36:21 +0200

    Update README

--- src/server.tsx
+++ src/server.tsx
@@ -28,7 +28,7 @@ server
     <head>
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta charset="utf-8" />
-        <title>Welcome to Razzle</title>
+        <title>Superpower</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         ${assets.client.css ? `<link rel="stylesheet" href="${assets.client.css}">` : ''}
         ${
